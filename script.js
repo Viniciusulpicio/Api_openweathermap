c216c488428d4a643fcc33789c3315a96aa4a1c5
Viniciusulpicio <viniciusulpicio@gmail.comgit config --global user.email git config --global user.email viniciusulpicio@gmail.com>
2024-03-01 08:54:49 -0300

    Legal

--- script.js
+++ script.js
@@ -1,102 +1,109 @@
 document
-  .getElementById("formClima")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+.getElementById("formClima")
+.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const city = document.getElementById("cityInput").value;
+  const city = document.getElementById("cityInput").value;
 
-    fetch(`http://localhost:3000/climatempo/${city}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const tempoResult = document.getElementById("climaResult");
-        const iconeClima = document.getElementById("iconeClima");
+  fetch(`http://localhost:3000/climatempo/${city}`)
+    .then(response => response.json())
+    .then(data => {
+      const tempoResult = document.getElementById("climaResult");
+      const iconeClima = document.getElementById("iconeClima");
 
-        function getIconeClima(clima) {
-            // Mapeia as condições climáticas para os nomes de arquivo dos ícones
-            const icones = {
-              poucas_nuvens: "/img/icone_poucas_nuvens.png",
-              nuvens_dispersas: "/img/icone_nuvens_dispersas.png",
-              nublado: "/img/icone_nublado.png",
-              nuvens_cheias: "/img/icone_nuvens_cheias.png",
-              ceu_limpo: "/img/icone_ceu_limpo.png",
-              chuva_leve: "/img/icone_chuva_leve.png",
-              chuvisco_intenso: "/img/icone_chuvisco_intenso.png",
-              chuva_moderada: "/img/icone_chuva_moderada.png",
-              chuva_rapida: "/img/icone_chuva_rapida.png",
-              nevoa: "/img/icone_nevoa.png",
-              tempestade: "/img/icone_tempestade.png",
-              neve: "/img/icone_neve.png",
-            };
-          
-            // Obtém o nome do arquivo do ícone com base no clima
-            const iconeFileName = icones[clima.toLowerCase()] || "/img/chuva_leve.png";
-          
-            // Retorna o caminho completo do ícone
-            return `/img/${iconeFileName}`;
-          }
-          
+      console.log(data.Clima);
+     
+      var imagemSrc;
 
-        if (data.Temperatura) {
-            console.log(iconeFileName);
-          tempoResult.innerHTML = `
-            <div class="card-temp" id="umidade_icon">
-                <img class="icon-temp" src="/img/Umidade.png" alt="">
-                <div>
+        if (data.Clima === 'Poucas Nuvens') {
+          imagemSrc = "./img/poucas_nuvens.png";
+        } else if (clima.toLowerCase() === 'Nuvens Dispersas') {
+          imagemSrc = "./img/nuvens_dispersas.png";
+        } else if (clima.toLowerCase() === 'Nublado') {
+          imagemSrc = "./img/nublado.png";
+        } else if (clima.toLowerCase() === 'Nuvens Cheias') {
+          imagemSrc = "./img/nuvens_cheias.png";
+        } else if (clima.toLowerCase() === 'Céu Limpo') {
+          imagemSrc = "./img/ceu_limpo.png";
+        } else if (clima.toLowerCase() === 'Chuva Leve') {
+          imagemSrc = "./img/chuva_leve.png";
+        } else if (clima.toLowerCase() === 'Chuvisco Intenso') {
+          imagemSrc = "./img/chuvisco_intenso.png";
+        } else if (clima.toLowerCase() === 'Chuva Moderada') {
+          imagemSrc = "./img/chuva_moderada.png";
+        } else if (clima.toLowerCase() === 'Chuva Rapida') {
+          imagemSrc = "./img/chuva_rapida.png";
+        } else if (clima.toLowerCase() === 'Névoa') {
+          imagemSrc = "./img/nevoa.png";
+        } else if (clima.toLowerCase() === 'Tempestade') {
+          imagemSrc = "./img/tempestade.png";
+        } else if (clima.toLowerCase() === 'Neve') {
+          imagemSrc = "./img/neve.png";
+        } else {
+          console.log("desgraça")
+        }
+      
+      if (data.Temperatura) {
+        tempoResult.innerHTML = `
+          <div class="card-temp" id="umidade_icon">
+              <img class="iconvento" src="./img/Umidade.png" alt="">
+              
+              <div>
                 <p class="numero">${data.Umidade}%</p>
                 <p>Umidade</p>
-                </div>
+              </div> <br />
 
 
+              <div class="card-temp">
+              
+              <img src='${imagemSrc}'>
 
-                <div class="card-temp">
-                Temperatura: ${data.Temperatura}°C
+                Temperatura: ${data.Temperatura}°C <br />
                 Descrição do Clima: ${data.Clima}
-                </div>
+              </div><br />
 
-                <img class="icon-temp" src="/img/Velocidade do Vento.png" alt="">
-                <div>
-                <p class="numero">${data.VelocidadeVento}%</p>
+              <img class="iconvento" src="./img/Velocidade do Vento.png" alt=""> <br />
+              
+              <div>
+                <p class="numero">${data.VelocidadeVento}Km/h</p>
                 <p>Velocidade do Vento</p>
+              </div><br />
 
+          </div>`;
 
-            </div>
-            </div>`;
-
-          // Altera o fundo com base nas condições climáticas
-          const fundoClimatico = getFundoClimatico(data.Clima);
-          if (fundoClimatico === "Escuro") {
-            document.documentElement.style.backgroundImage = `url(/img/fundo_escuro.png)`;
-          } else if (fundoClimatico === "Frio") {
-            document.documentElement.style.backgroundImage = `url(/img/fundo_frio.png)`;
-          }
-
-          // Set the weather icon based on the weather description
-          const iconeClimaSrc = getIconeClimaSrc(data.Clima);
-          if (iconeClimaSrc) {
-            iconeClima.src = iconeClimaSrc;
-          } else {
-            console.error(`Ícone do clima não encontrado para: ${data.Clima}`);
-          }
-        } else {
-          tempoResult.innerHTML = "Erro ao obter dados meteorológicos";
+        // Altera o fundo com base nas condições climáticas
+        const fundoClimatico = getFundoClimatico(data.Clima);
+        if (fundoClimatico === "Escuro") {
+          document.documentElement.style.backgroundImage = `url(./img/fundo_escuro.png)`;
+        } else if (fundoClimatico === "Frio") {
+          document.documentElement.style.backgroundImage = `url(./img/fundo_frio.png)`;
         }
-      })
-      .catch((error) => console.error("Erro ao obter dados:", error));
-  });
+
+        // Set the weather icon based on the weather description
+        const iconeClimaSrc = getIconeClimaSrc(data.Clima);
+        if (iconeClimaSrc) {
+          iconeClima.src = iconeClimaSrc;
+        } else {
+          console.error(`Ícone do clima não encontrado para: ${data.Clima}`);
+        }
+      } else {
+        tempoResult.innerHTML = "Erro ao obter dados meteorológicos";
+      }
+    })
+    .catch((error) => console.error("Erro ao obter dados:", error));
+});
 
 // Função para obter o tipo de fundo com base no clima
 function getFundoClimatico(clima) {
-  // Lógica para determinar o tipo de fundo com base no clima
-  if (
-    clima.toLowerCase().includes("tempestade") ||
-    clima.toLowerCase().includes("chuva")
-  ) {
-    // Se há tempestade ou chuva: Escuro
-    return "Escuro";
-  } else {
-    // Frio
-    return "Frio";
-  }
+// Lógica para determinar o tipo de fundo com base no clima
+if (
+  clima.toLowerCase().includes("tempestade") ||
+  clima.toLowerCase().includes("chuva")
+) {
+  // Se há tempestade ou chuva: Escuro
+  return "Escuro";
+} else {
+  // Frio
+  return "Frio";
 }
-
-
+}
